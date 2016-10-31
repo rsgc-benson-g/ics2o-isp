a166a1a91e06dcce0bdc77876a432dd6711b5c65
Benson <student@benson.local>
2016-10-31 10:34:27 -0400

    Working timer (negatives)

--- IntervalTimer/ViewController.swift
+++ IntervalTimer/ViewController.swift
@@ -9,26 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var timerValue: UILabel!
+
     
+    var timer = Timer()
+    //initial value
+    var timeLeft = 60
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func startTimer(_ sender: AnyObject) {
         
-        
+        //initilieze timer used to count down
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: Selector("decrement"), userInfo: nil, repeats: true)
     }
-
+    
+    //decreases timer value
+    func decrement(){
+        
+        timeLeft = timeLeft - 1
+        timerValue.text = "\(timeLeft)"
+    }
+    
 }
 
