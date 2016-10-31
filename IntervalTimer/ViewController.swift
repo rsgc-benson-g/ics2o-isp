396488c68c025b626612587af9cd2eff00b9d713
Benson <student@benson.local>
2016-10-31 10:20:26 -0400

    Established Connections

--- IntervalTimer/ViewController.swift
+++ IntervalTimer/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var timerValue: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startTimer(_ sender: AnyObject) {
+        
+        
+    }
 
 }
 
